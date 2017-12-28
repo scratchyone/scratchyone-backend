fda06f286b9bb7dcecfc1d588ec402eebc2cfe3a
scratchyone <scratchywon@gmail.com>
2017-12-28 09:44:40 -0800

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 <?php
 $ip = $_SERVER['HTTP_X_FORWARDED_FOR'][0];
 $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
-echo explode(",",$_SERVER['HTTP_X_FORWARDED_FOR']);
+echo explode(",",$_SERVER['HTTP_X_FORWARDED_FOR'])[0];
 echo "You live in ";
 echo $details->city;
 echo "!";
