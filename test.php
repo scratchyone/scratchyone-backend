e032452782bfd13f3dc6d680583fb25290dcba02
scratchyone <scratchywon@gmail.com>
2017-12-28 09:32:42 -0800

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 <?php
 $ip = $_SERVER['REMOTE_ADDR'];
 $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
-echo $_SERVER['REMOTE_ADDR'];
+echo $_SERVER['HTTP_X_FORWARDED_FOR'];
 echo "You live in ";
 echo $details->city;
 echo "!";
