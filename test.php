85f59636d125146b378bff80e00ddd6f348e361f
scratchyone <scratchywon@gmail.com>
2017-12-28 09:29:13 -0800

    Update test.php

--- test.php
+++ test.php
@@ -2,6 +2,7 @@
 <?php
 $ip = $_SERVER['REMOTE_ADDR'];
 $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
+echo rand(1,5);
 echo "You live in ";
 echo $details->city;
 echo "!";
